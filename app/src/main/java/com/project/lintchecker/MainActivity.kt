c9d70fa83fdfe9092e20d0766c12d8939cc707b7
Vishwanath Patil <vishwanath26.patil@gmail.com>
2026-02-17 18:39:21 +0530

    bring text in center

--- app/src/main/java/com/project/lintchecker/MainActivity.kt
+++ app/src/main/java/com/project/lintchecker/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.project.lintchecker.ui.theme.LintCheckerTheme
@@ -35,10 +38,16 @@ fun Greeting(
     name: String,
     modifier: Modifier = Modifier,
 ) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+        )
+    }
 }
 
 @Preview(showBackground = true)
