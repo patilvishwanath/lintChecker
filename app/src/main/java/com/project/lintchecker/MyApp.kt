e559124a1cf85ec399df5924fb412eca517a0423
vp <vishwanath26.patil@gmail.com>
2026-02-17 18:29:50 +0530

    Code updates (#1)
    * Added Screenshot check \n Updated ktlint cli fpr pre-commit
    
    * - updated lint-check.yml
    
    * - minor code change
    
    * - minor code change for lint check
    
    * - minor code change for lint check
    
    * - minor code change for editorconfig

---
+++ app/src/main/java/com/project/lintchecker/MyApp.kt
@@ -0,0 +1,39 @@
+package com.project.lintchecker
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        registerActivityLifecycleCallbacks(ScreenshotActivityLifecycleCallbacks())
+    }
+
+    private class ScreenshotActivityLifecycleCallbacks : ActivityLifecycleCallbacks {
+        override fun onActivityResumed(activity: Activity) {
+            ScreenshotDetector.register(activity)
+        }
+
+        override fun onActivityPaused(activity: Activity) {
+            ScreenshotDetector.unregister()
+        }
+
+        override fun onActivityCreated(
+            activity: Activity,
+            savedInstanceState: Bundle?,
+        ) {}
+
+        override fun onActivityStarted(activity: Activity) {}
+
+        override fun onActivityStopped(activity: Activity) {}
+
+        override fun onActivitySaveInstanceState(
+            activity: Activity,
+            outState: Bundle,
+        ) {}
+
+        override fun onActivityDestroyed(activity: Activity) {}
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,18 @@
+@file:Suppress("ktlint:standard:indent")
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.kotlin.android) apply false
   id("org.jlleitschuh.gradle.ktlint") version "14.0.1" apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    // Optionally configure plugin
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+    }
 }
